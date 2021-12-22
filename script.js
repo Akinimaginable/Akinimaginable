8e1fd23a3c3c9c672f4b624bdfdbb55079535d0f
Akinimaginable 秋 <38192770+Akinimaginable@users.noreply.github.com>
2021-12-22 04:01:17 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function en() {
+  document.getElementById("t").innerHTML =
+    "I like to create things using lines of code";
+}
+function fr() {
+  document.getElementById("t").innerHTML =
+    "J'aime créer des choses en utilisant des lignes de code";
+}
+function nl() {
+  document.getElementById("t").innerHTML =
+    "Ik hou ervan om dingen te maken met behulp van lijnen van code";
+}
