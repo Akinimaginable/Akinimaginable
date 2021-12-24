a74a27cf657c0fcccdc4e40cbf59abf884bc3301
Akinimaginable <38192770+Akinimaginable@users.noreply.github.com>
2021-12-24 12:38:26 +0100

    Add favicons

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+let lang = "en";
+
 function en() {
   document.getElementById("t").innerHTML =
     "I like to create things using lines of code";
+  lang = "en";
 }
 function fr() {
   document.getElementById("t").innerHTML =
     "J'aime créer des choses en utilisant des lignes de code";
+  lang = "fr";
 }
 function nl() {
   document.getElementById("t").innerHTML =
     "Ik hou ervan om dingen te maken met behulp van lijnen van code";
+  lang = "nl";
 }
